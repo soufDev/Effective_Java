5b3750a3c58f91876ceace90facb332b98b61c44
soufiane <ait.akkache.soufiane@gmail.com>
2018-02-13 09:24:23 +0100

    item 11 finished

--- src/item11/HashTable.java
+++ src/item11/HashTable.java
@@ -1,8 +1,10 @@
 package item11;
 
 
+import java.util.Arrays;
+
 public class HashTable implements Cloneable {
-    private Entry[] buckets = new Entry[8];
+    private Entry[] buckets = new Entry[]{};
 
     private static class Entry {
         final Object key;
@@ -14,17 +16,46 @@ public class HashTable implements Cloneable {
             this.value = value;
             this.next = next;
         }
+
+        // copy the linked list header by this entry
+        Entry deepCopy() {
+            Entry result = new Entry(key, value, next);
+            for (Entry p = result ; p.next != null ; p = p.next)
+                p.next = new Entry(p.next.key, p.next.value, p.next.next);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" +
+                "buckets=" + Arrays.toString(buckets) +
+                '}';
     }
 
     @Override
     public HashTable clone() {
         try {
             HashTable result = (HashTable) super.clone();
-            result.buckets = buckets.clone();
+            result.buckets = new Entry[buckets.length];
+            for (int i = 0 ; i<buckets.length ; i++)
+                if (buckets[i] != null)
+                    result.buckets[i] = buckets[i].deepCopy();
             return result;
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
         }
+
+
     }
 
     public static void main(String[] arg) {
