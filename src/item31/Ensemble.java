d7dae94336063188fa0d97951b3beaec367cd2bd
soufiane <ait.akkache.soufiane@gmail.com>
2018-02-26 17:43:52 +0100

    item31

---
+++ src/item31/Ensemble.java
@@ -0,0 +1,20 @@
+package item31;
+
+public enum Ensemble {
+    SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5), SEXTET(6),
+    SEPTET(7), OCTET(8), NONET(9), DECTET(10);
+
+    private final int numberOfMusicians;
+
+    Ensemble(int size) {
+        this.numberOfMusicians = size;
+    }
+
+    public int numberOfMusicians() {
+        return ordinal();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Ensemble.values()[0].numberOfMusicians());
+    }
+}
